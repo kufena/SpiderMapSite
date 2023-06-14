674d0b92beb7709518cbd56233ef4357db6e9a3b
Andrew Douglas <fairtomiddlingster@gmail.com>
2023-06-14 14:51:10 +0100

    Processing of the gate house list - led to removing some spider records but we can do better I think.

---
+++ ProcessGateHouseList/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var alllines = File.ReadAllLines(args[0]);
+Dictionary<string, int> counts = new Dictionary<string, int>();
+
+foreach (var line in alllines)
+{
+    var splits = line.Split(",");
+    if (splits.Length > 3)
+    {
+        // should be date, determined, thought to be, comment
+        var thoughtToBe = splits[2].ToLower().Trim();
+        if (counts.ContainsKey(thoughtToBe)) counts[thoughtToBe] += 1;
+        else counts.Add(thoughtToBe, 1);
+    }
+}
+
+Console.WriteLine($"Actual different keys = {counts.Keys.Count}");
+var keys = counts.Keys.ToArray();
+Array.Sort(keys);
+
+foreach (var key in keys)
+{
+    Console.WriteLine($"{key} = {counts[key]}");
+}
